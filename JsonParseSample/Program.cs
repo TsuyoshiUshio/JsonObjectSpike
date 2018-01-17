f861c12fe66ddd3701ae9bed6c106b781216d234
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-01-17 15:38:33 +0900

    initial commit

--- JsonParseSample/Program.cs
+++ JsonParseSample/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
 
 namespace JsonParseSample
 {
@@ -6,7 +11,30 @@ namespace JsonParseSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+
+            Console.WriteLine("Serialize------");
+            var list = new List<JObject>();
+            for(var i = 0; i < 10; i++)
+            {
+                list.Add(JObject.Parse("{" + $"'counter-{i.ToString("00")}': '{Guid.NewGuid()}'" + "}"));
+            }
+            var serialized = JsonConvert.SerializeObject(list);
+            Console.WriteLine(serialized);
+            Console.ReadLine();
+
+            Console.WriteLine("DeSerialize-------");
+
+            var restored = JsonConvert.DeserializeObject<List<JObject>>(serialized);
+
+            foreach (var obj in restored)
+            {
+                foreach (var value in obj.Values())
+                {
+                    Console.WriteLine(value.Value<string>());
+                }
+            }
+
+            Console.ReadLine();
+           }
     }
 }
